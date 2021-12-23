2754f0df11d54b8f92c110b1a23ba53000c7a25e
NTD <NTD@192.168.1.117>
2021-12-23 15:58:05 +0700

    Afternoon 23/12/2021, Complete Adding CSS,js by JSTL

--- SpringMVCSample/src/main/java/SpringMVCSample/UserController/HomeController.java
+++ SpringMVCSample/src/main/java/SpringMVCSample/UserController/HomeController.java
@@ -1,13 +1,15 @@
-package SpringMVCSample.Controller;
+package SpringMVCSample.UserController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value= {"/","/trang-chu"}, method = RequestMethod.GET)
-	public String Index() {
-		return"user/index";
+	public ModelAndView Index() {
+		ModelAndView mv = new ModelAndView("user/index");
+		return mv;
 	};
 }
